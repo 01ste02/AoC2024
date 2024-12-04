a43aa37c29252b4bfda9d731449676c574f100c3
Oskar Stenberg <01ste02@gmail.com>
2024-12-04 13:47:53 +0100

    Done part1

--- day2/part1.c
+++ day2/part1.c
@@ -17,38 +17,39 @@ int main(int argc, char *argv[]) {
 	int lastDigit = -1;
 	int diff = 0;
 	
-	int n;
+	int n = 0;
 	int dir = 0;
 	while((ch = fgetc(input)) != EOF) {
+		if (safe && ch != ' ' && ch != '\n') {
+			n = n * 10 + (ch - 48);
+		} else if (safe && lastDigit != -1) {
+			if (dir == 0) {
+				dir = (lastDigit < n) ? 1 : -1;
+			}
+
+			diff = n - lastDigit;
+			//printf("Dir: %d, n: %d, last: %d, diff: %d, safe: ", dir, n, lastDigit, diff);
+			if (diff > 3 || diff < -3 || diff == 0 ||
+			    (dir < 0 && diff > 0) ||
+			    (dir > 0 && diff < 0)) {
+				safe = 0;
+			}
+
+			//printf("%d\n", safe);
+		}
+
 		if (ch == '\n') {
+			//printf("n: %d, last: %d\n", n, lastDigit);
 			if (safe && dir != 0)
 				numSafe++;
 			safe = 1;
 			lastDigit = -1;
 			dir = 0;
-			printf("\n");
-		} else if (ch != ' ' && safe) {
-			n = ch - 48;
-			diff = n - lastDigit;
-			
-			if (lastDigit != -1) {
-				if (dir == 0) {
-					dir = (lastDigit < n) ? 1 : -1;
-				}
-
-				printf("Dir: %d, n: %d, last: %d, diff: %d, safe: ", dir, n, lastDigit, diff);
-			
-				if (diff > 3 || diff < -3 ||
-				    (diff > -1 && diff < 1) ||
-				    (dir < 0 && diff > 0) ||
-				    (dir > 0 && diff < 0)) {
-					safe = 0;
-				}
-
-				printf("%d\n", safe);
-			}
-
+			n = 0;
+			//printf("\n");
+		} else if (ch == ' ') {
 			lastDigit = n;
+			n = 0;
 		}
 	}
 
